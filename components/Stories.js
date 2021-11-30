162ee503b328097f9efc8f89a2ad81ee7931c7f2
jddadev <joethebartenderpdx@gmail.com>
2021-11-30 10:20:40 -0800

    scrollbar implented in stories, hiddeon on overall site. still issues with images

--- components/Stories.js
+++ components/Stories.js
@@ -15,7 +15,7 @@ function Stories() {
   }, []);
 
   return (
-    <div>
+    <div className="flex space-x-2 p-6 bg-white mt-8 border border-gray-200 rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
       {suggestions?.map((profile) => (
         <Story
           key={profile.id}
