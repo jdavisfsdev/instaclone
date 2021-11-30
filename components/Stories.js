617f03c7745b0b0fe6d40fcf9529cadb419f9f28
jddadev <joethebartenderpdx@gmail.com>
2021-11-29 22:06:17 -0800

    fake user data generated on load

--- components/Stories.js
+++ components/Stories.js
@@ -1,4 +1,14 @@
+import faker from 'faker';
+import { useEffect } from 'react';
+
 function Stories() {
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      //   contextualCard creates fake data including name, avatar, address etc...
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+  }, []);
   return (
     <div>
       {/* Story */}
