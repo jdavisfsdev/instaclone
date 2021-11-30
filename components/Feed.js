7409d3364aef7b9172a0742f43deeb59095960f5
jddadev <joethebartenderpdx@gmail.com>
2021-11-29 21:53:26 -0800

    Feed component created, sudo coded

---
+++ components/Feed.js
@@ -0,0 +1,17 @@
+function Feed() {
+  return (
+    <main>
+      {/* Section */}
+      <section></section>
+      {/* Stories */}
+      {/* Posts */}
+
+      {/* Section */}
+      <section></section>
+      {/* Mini Profile */}
+      {/* Suggestions */}
+    </main>
+  );
+}
+
+export default Feed;
