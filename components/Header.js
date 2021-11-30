f730a777dec9d4fba0cae2df5cc08b08f6018ad4
jddadev <joethebartenderpdx@gmail.com>
2021-11-29 20:08:22 -0800

    installed tailwind forms

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ function Header() {
     <header>
       {/* Left */}
       <div className="flex justify-between max-w-6xl">
-        <div className="relative hidden lg:inline-grid w-24 h-24 cursor-pointer">
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
             src="https://links.papareact.com/ocw"
             layout="fill"
@@ -21,26 +21,30 @@ function Header() {
           />
         </div>
 
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src="https://links.papareact.com/jjm"
             layout="fill"
             objectFit="contain"
           />
         </div>
-      </div>
 
-      {/* Middle */}
-      <div className="relative mt-1 p-3 rounded-md bg-red-500 ">
-        {/* Search */}
-        <div className="absolute inset-y-0 flex items-center pointer-events-none">
-          <SearchIcon className="h-5 w-5 text-gray-400" />
+        {/* Middle */}
+        <div className="relative mt-1 p-3 rounded-md">
+          {/* Search */}
+          <div className="absolute inset-y-0 flex items-center pointer-events-none">
+            <SearchIcon className="h-5 w-5 text-gray-400" />
+          </div>
+          <input
+            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+            type="text"
+            placeholder="search"
+          />
         </div>
-        <input type="text" placeholder="search" />
-      </div>
 
-      {/* Right */}
-      <div></div>
+        {/* Right */}
+        <div></div>
+      </div>
     </header>
   );
 }
