c2bda2b6a2ade9ca19c2bd96cbd06d9130a6b0c7
jddadev <joethebartenderpdx@gmail.com>
2021-11-29 19:24:59 -0800

    heroicons imported, search field created

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,17 @@
 import Image from 'next/image';
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/outline';
 
 function Header() {
   return (
     <header>
+      {/* Left */}
       <div className="flex justify-between max-w-6xl">
         <div className="relative hidden lg:inline-grid w-24 h-24 cursor-pointer">
           <Image
@@ -20,6 +29,18 @@ function Header() {
           />
         </div>
       </div>
+
+      {/* Middle */}
+      <div className="relative mt-1 p-3 rounded-md bg-red-500 ">
+        {/* Search */}
+        <div className="absolute inset-y-0 flex items-center pointer-events-none">
+          <SearchIcon className="h-5 w-5 text-gray-400" />
+        </div>
+        <input type="text" placeholder="search" />
+      </div>
+
+      {/* Right */}
+      <div></div>
     </header>
   );
 }
