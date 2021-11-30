d829c862fad81de6b9e43c4a81ce727eac68706b
jddadev <joethebartenderpdx@gmail.com>
2021-11-29 21:23:01 -0800

    created global css file, created navButton class for nav icons

--- components/Header.js
+++ components/Header.js
@@ -7,12 +7,14 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from '@heroicons/react/outline';
+import { HomeIcon } from '@heroicons/react/solid';
+import Home from '../pages';
 
 function Header() {
   return (
     <header>
       {/* Left */}
-      <div className="flex justify-between max-w-6xl">
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
             src="https://links.papareact.com/ocw"
@@ -30,20 +32,27 @@ function Header() {
         </div>
 
         {/* Middle */}
-        <div className="relative mt-1 p-3 rounded-md">
-          {/* Search */}
-          <div className="absolute inset-y-0 flex items-center pointer-events-none">
-            <SearchIcon className="h-5 w-5 text-gray-400" />
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            {/* Search */}
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-400" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="search"
+            />
           </div>
-          <input
-            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
-            type="text"
-            placeholder="search"
-          />
         </div>
 
         {/* Right */}
-        <div></div>
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navButton" />
+          <MenuIcon className="h-6 md:hidden  cursor-pointer" />
+
+          <PaperAirplaneIcon className="navButton" />
+        </div>
       </div>
     </header>
   );
