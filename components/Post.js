4a143889bca93011647f3a649dd0a182940ecaf2
jddadev <joethebartenderpdx@gmail.com>
2021-12-01 22:43:34 -0800

    Header for Post all styled up

--- components/Post.js
+++ components/Post.js
@@ -11,9 +11,14 @@ function Post({ id, username, userImg, img, caption }) {
   return (
     <div>
       {/* Header */}
-      <div>
-        <img src={userImg} alt="" />
-        <p>{username}</p>
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          alt="User Image"
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
       </div>
 
       {/* img */}
