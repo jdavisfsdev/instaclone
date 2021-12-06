411cb79e603ea71737cbc45c19c032841cef455a
jddadev <joethebartenderpdx@gmail.com>
2021-12-05 21:41:05 -0800

    MiniProfile setup with static data

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,5 +1,20 @@
 function MiniProfile() {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-between mt-14 ml-10">
+      <img
+        className="w-16 h-16 rounded-full border p-[2px]"
+        src="https://links.papareact.com/3ke"
+        alt=""
+      />
+
+      <div>
+        <h2>jdpdx1</h2>
+        <h3>Welcome to Instagram</h3>
+      </div>
+
+      <button>Sign Out</button>
+    </div>
+  );
 }
 
 export default MiniProfile;
