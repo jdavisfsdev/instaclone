bb9ff35c10b481dc8efc36a7a13721d894950910
jddadev <joethebartenderpdx@gmail.com>
2021-12-07 18:39:39 -0800

    added user to stories

--- components/Stories.js
+++ components/Stories.js
@@ -1,8 +1,10 @@
 import faker from 'faker';
+import { useSession } from 'next-auth/react';
 import { Profiler, useEffect, useState } from 'react';
 import Story from './Story';
 
 function Stories() {
+  const { data: session } = useSession();
   const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
@@ -16,6 +18,9 @@ function Stories() {
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border border-gray-200 rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story img={session.user.image} username={session.user.username} />
+      )}
       {suggestions?.map((profile) => (
         <Story
           key={profile.id}
