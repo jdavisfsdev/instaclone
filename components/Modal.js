b32b7e9761234ef14497a6249ea0b4387b8505e3
jddadev <joethebartenderpdx@gmail.com>
2021-12-09 15:15:00 -0800

    added TransitionRoot and TransitionChild with its proper styling

--- components/Modal.js
+++ components/Modal.js
@@ -7,7 +7,25 @@ function Modal() {
   const [open, setOpen] = useRecoilState(modalState);
   return (
     <Transition.Root show={open} as={Fragment}>
-      <h1></h1>
+      <Dialog
+        as="div"
+        className="fixed z-10 inset-0 overflow-y-auto"
+        onClose={setOpen}
+      >
+        <div className="flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+        </div>
+      </Dialog>
     </Transition.Root>
   );
 }
